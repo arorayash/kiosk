2bf9c6c87e701e5838811de4fd9f7eb5b85bf4cb
Yash Arora <arorayash.thisisus@gmail.com>
2017-01-06 08:37:27 +0530

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    context : __dirname + "/app",
+
+    entry : {
+        javascript : "./scripts/app.js",
+        html : "./index.html"
+    },
+
+    resolve : {
+        extensions : ['', '.js', '.jsx', '.json']
+    },
+
+    module : {
+        loaders : [
+            {
+                test : /\.jsx?$/,
+                exclude : /node_modules/ ,
+                loaders : ["react-hot", "babel-loader"]
+            },
+            {
+                test : /\.html$/,
+                loader : "file?name=[name].[ext]"
+            }
+        ]
+    },
+
+    output : {
+        filename : "app.js",
+        path : __dirname + "/dist"
+    }
+}
