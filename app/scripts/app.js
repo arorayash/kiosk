141febe143bd556cff57e417ac3d7bcc289146d6
Yash Arora <arorayash.thisisus@gmail.com>
2017-01-08 11:22:23 +0530

    added sample feeds functionality

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,13 +7,41 @@ import About from "./views/About";
 
 import { Router, Route, Link, browserHistory } from "react-router";
 
-import h from "./helpers/helper";
+import sample from "./helpers/sample";
 
 import "../styles/app.scss"
 
 const AppMounter = document.getElementById("app");
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state =  {
+            feeds : {},
+            readings : {}
+        }
+    }
+
+    addFeed(feed) {
+        let timeStamp = (new Date()).getTime();
+        this.state.feeds["feed-" + timeStamp] = feed;
+
+        this.setState({feeds : this.state.feeds});
+    }
+
+    loadSampleFeeds(event) {
+        this.setState({feeds : sample});
+        //console.log(sample);
+    }
+
+    renderFeedList() {
+        return Object.keys(this.state.feeds).map(this.renderOneFeed.bind(this));
+    }
+
+    renderOneFeed(key) {
+        return <Feed key={key} index={key} details={this.state.feeds[key]} />;
+    }
+
     render() {
         return (
             <div className="mainApp">
@@ -24,20 +52,25 @@ class App extends React.Component {
                         <li><Link className="link" to="/rooms">Rooms</Link></li>
                     </ul>
                 </nav>*/}
-                <Main tagline="A curated list of resources"/>
+                <Main tagline="A curated list of resources" renderFeed={this.renderFeedList.bind(this)}/>
                 <ReadingList/>
-                <Contribution/>
+                <Contribution addFeed={this.addFeed.bind(this)} loadFeed={this.loadSampleFeeds.bind(this)}/>
             </div>
         );
     }
 }
 
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render () {
         return (
             <div className="main">
                 <h2>Main</h2>
                 <h4>{this.props.tagline}</h4>
+                <ul>{this.props.renderFeed()}</ul>
             </div>
         )
     }
@@ -58,6 +91,8 @@ class Contribution extends React.Component {
         return (
             <div className="contribution">
                 <h2>Contribution</h2>
+                <ContributionForm {...this.props}/>
+                <button onClick={this.props.loadFeed}>Add sample feeds</button>
             </div>
         )
     }
@@ -67,6 +102,53 @@ class NotFound extends React.Component {
     render () {
         return (
             <h2>Error 404!</h2>
+        )
+    }
+}
+
+class ContributionForm extends React.Component {
+    makeContribution(event) {
+        event.preventDefault();
+        console.log("submitting");
+
+        var contribution = {
+            name: this.refs.name.value,
+            link: this.refs.link.value,
+            img: this.refs.img.value,
+            desc: this.refs.desc.value
+        }
+
+        this.props.addFeed(contribution);
+
+        this.refs.contributionForm.reset();
+    }
+
+
+    render () {
+        return (
+            <form ref="contributionForm" onSubmit={this.makeContribution.bind(this)}>
+                <input type="text" ref="name" placeholder="Name" required/>
+                <input type="text" ref="link" placeholder="Link" required/>
+                <input type="img" ref="file" accept="image/*" placeholder="Link icon"/>
+                <textarea type="text" ref="desc" placeholder="Description"></textarea>
+                <button type="submit">Submit</button>
+            </form>
+        )
+    }
+}
+
+class Feed extends React.Component {
+    render() {
+        return (
+            <li className="feed">
+                <hr/>
+                <h3>{this.props.details.name}</h3>
+                <img src={this.props.details.img} alt=""/>
+                <h4>
+                    <a href={this.props.details.link}>Open Article</a>
+                </h4>
+                <hr/>
+            </li>
         )
     }
 }
---
+++ app/scripts/helpers/sample.js
@@ -0,0 +1,20 @@
+module.exports = {
+feed1 : {
+    name: "Self-driving cars",
+    link: "http://app.muz.li/go?link=https%3A%2F%2Fwww.wired.com%2F2017%2F01%2Fnissans-self-driving-teleoperation%2F&source=hacker_news",
+    img: "https://d37zglfmscqwcx.cloudfront.net/00e17a2660d2f3ee9f3bb8eb95f9912c",
+    desc: ""
+},
+feed2 : {
+    name: "Self-driving cars",
+    link: "http://app.muz.li/go?link=http%3A%2F%2Fmashable.com%2F2017%2F01%2F07%2Fcarrie-fisher-debbie-reynolds-bright-lights-documentary-hbo-quotes%2F&source=mashable",
+    img: "http://i.amz.mshcdn.com/2CCwYOygNs9IuywNU7UXLja2cHM=/575x323/filters:quality(90)/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fcard%2Fimage%2F340349%2F53dc4bc2-a2ba-4572-805c-c20b86bca549.jpg",
+    desc: ""
+},
+feed3 : {
+     name: "Growth of Bots in 2017",
+     link: "http://app.muz.li/go?link=https%3A%2F%2Fwww.fastcompany.com%2F3066706%2Frobot-revolution%2Fwill-bots-finally-start-to-grow-up-in-2017&source=fastcompany",
+     img: "https://d37zglfmscqwcx.cloudfront.net/e89e60104b3532e5791fbb781a10c3a3",
+     desc: ""
+}
+}
